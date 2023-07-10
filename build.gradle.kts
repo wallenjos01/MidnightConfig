9cae7e0c4f5b5f7ee1459e9dc5f20c570d5299cf
M1dnightNinja <wallenjos01@gmail.com>
2023-07-10 13:07:52 -0600

    Update to Gradle 8.2. Migrate to Kotlin Gradle DSL

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+import java.net.URI
+
+plugins {
+    id("java")
+    id("java-library")
+    id("maven-publish")
+}
+
+group = "org.wallentines"
+version = "1.0.0"
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
+
+java.withSourcesJar()
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+}
+
+tasks.test {
+    useJUnitPlatform()
+    workingDir("run/test")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group as String
+            version = version as String
+            from(components["java"])
+        }
+    }
+    repositories {
+        if (project.hasProperty("pubUrl")) {
+            maven {
+                name = "pub"
+                url = URI.create(project.properties["pubUrl"] as String)
+                credentials(PasswordCredentials::class.java)
+            }
+        }
+    }
+}
