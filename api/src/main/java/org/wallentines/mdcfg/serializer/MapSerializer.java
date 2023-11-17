8e7337f711e145d7ad8c9ea461caecbd787c6854
wallenjos01 <wallenjos01@gmail.com>
2023-11-16 22:30:10 -0700

    Backwards compatibility

--- api/src/main/java/org/wallentines/mdcfg/serializer/MapSerializer.java
+++ api/src/main/java/org/wallentines/mdcfg/serializer/MapSerializer.java
@@ -3,6 +3,7 @@ package org.wallentines.mdcfg.serializer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 /**
  * A serializer for key-value pairs of objects
@@ -24,6 +25,19 @@ public class MapSerializer<K, V> implements Serializer<Map<K, V>> {
         this(keySerializer, valueSerializer, (k, str) -> true);
     }
 
+    /**
+     * Creates a MapSerializer with the given key and value serializers, which reports errors to the given function
+     * @param keySerializer The serializer to use to serialize keys
+     * @param valueSerializer The serializer to use to map values
+     * @param onError The function to call when an error is encountered. If this function returns true, (de)serializing
+     *                will be stopped with an error
+     */
+    @Deprecated
+    public MapSerializer(InlineSerializer<K> keySerializer, Serializer<V> valueSerializer, Function<String, Boolean> onError) {
+        this.keySerializer = keySerializer;
+        this.valueSerializer = valueSerializer;
+        this.onError = (k, str) -> onError.apply(str);
+    }
     /**
      * Creates a MapSerializer with the given key and value serializers, which reports errors to the given function
      * @param keySerializer The serializer to use to serialize keys
