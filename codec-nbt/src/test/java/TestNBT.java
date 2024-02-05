d35faa7dd01e85ca0c325ebecc1f0bc1bcc7effd
wallenjos01 <wallenjos01@gmail.com>
2024-02-05 16:47:34 -0700

    Add boolean test for NBT

--- codec-nbt/src/test/java/TestNBT.java
+++ codec-nbt/src/test/java/TestNBT.java
@@ -2,8 +2,12 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.wallentines.mdcfg.ConfigObject;
+import org.wallentines.mdcfg.ConfigPrimitive;
+import org.wallentines.mdcfg.codec.DecodeException;
+import org.wallentines.mdcfg.codec.EncodeException;
 import org.wallentines.mdcfg.codec.NBTCodec;
 import org.wallentines.mdcfg.serializer.ConfigContext;
+import org.wallentines.mdcfg.serializer.Serializer;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -65,6 +69,31 @@ public class TestNBT {
         }
 
         Assertions.assertEquals(decoded, reDecoded);
+
+    }
+
+    @Test
+    public void testBooleans() {
+
+        NBTCodec codec = new NBTCodec(false);
+
+        byte[] value;
+        try(ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+            codec.encode(ConfigContext.INSTANCE, new ConfigPrimitive(true), bos);
+            value = bos.toByteArray();
+        } catch (IOException | EncodeException ex) {
+            Assertions.fail("An exception occurred while encoding a boolean!");
+            return;
+        }
+
+        try(ByteArrayInputStream bis = new ByteArrayInputStream(value)) {
+
+            boolean bl = codec.decode(ConfigContext.INSTANCE, Serializer.BOOLEAN, bis).getOrThrow();
+            Assertions.assertTrue(bl);
+
+        } catch(IOException | DecodeException ex) {
+            Assertions.fail("An exception occurred while decoding boolean!");
+        }
 
     }
 
