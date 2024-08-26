95a471897b43461480c262ed44e911774ab35f35
wallenjos01 <wallenjos01@gmail.com>
2024-08-26 04:37:55 +0000

    Add foojay for automatic toolchain download

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,10 @@ pluginManagement {
     includeBuild("gradle/plugins")
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 include("api")
 include("api-sql")
 
