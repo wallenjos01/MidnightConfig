8f3b09b5da2f18b0d900b9ee0debc96a937c2bb6
wallenjos01 <wallenjos01@gmail.com>
2025-08-21 16:18:19 -0600

    Make presets use environment serializer. Test mariadb preset

--- api-sql/src/main/java/org/wallentines/mdcfg/sql/DatabasePreset.java
+++ api-sql/src/main/java/org/wallentines/mdcfg/sql/DatabasePreset.java
@@ -1,6 +1,5 @@
 package org.wallentines.mdcfg.sql;
 
-import java.util.Optional;
 import org.wallentines.mdcfg.ConfigSection;
 import org.wallentines.mdcfg.serializer.ConfigContext;
 import org.wallentines.mdcfg.serializer.ObjectSerializer;
@@ -9,34 +8,33 @@ import org.wallentines.mdcfg.serializer.Serializer;
 
 public class DatabasePreset {
 
-    private final PresetParameter driver;
-    private final PresetParameter url;
-    private final PresetParameter username;
-    private final PresetParameter password;
-    private final PresetParameter database;
-    private final PresetParameter tablePrefix;
+    private final String driver;
+    private final String url;
+    private final String username;
+    private final String password;
+    private final String database;
+    private final String tablePrefix;
     private final ConfigSection parameters;
 
-    public String driver() { return driver.get(); }
+    public String driver() { return driver; }
     public String url() {
-        String baseUrl = url.get();
+        String baseUrl = url;
         String db = database();
         if (db != null) {
             baseUrl += "/" + db;
         }
         return baseUrl;
     }
-    public String username() { return username.get(); }
-    public String password() { return password.get(); }
-    public String database() { return database.get(); }
-    public String tablePrefix() { return tablePrefix.get(); }
+    public String username() { return username; }
+    public String password() { return password; }
+    public String database() { return database; }
+    public String tablePrefix() { return tablePrefix; }
     public ConfigSection parameters() { return parameters; }
 
-    public String baseUrl() { return url.get(); }
+    public String baseUrl() { return url; }
 
-    public DatabasePreset(PresetParameter driver, PresetParameter url,
-                          PresetParameter username, PresetParameter password,
-                          PresetParameter database, PresetParameter tablePrefix,
+    public DatabasePreset(String driver, String url, String username,
+                          String password, String database, String tablePrefix,
                           ConfigSection parameters) {
         this.driver = driver;
         this.url = url;
@@ -47,62 +45,22 @@ public class DatabasePreset {
         this.parameters = parameters;
     }
 
-    public DatabasePreset(String driver, String url, String username,
-                          String password, String database, String tablePrefix,
-                          ConfigSection parameters) {
-        this(new PresetParameter(driver), new PresetParameter(url),
-             new PresetParameter(username), new PresetParameter(password),
-             new PresetParameter(database), new PresetParameter(tablePrefix),
-             parameters);
-    }
-
-    public static class PresetParameter {
-
-        public final String value;
-        public final String env;
-
-        public PresetParameter(String value, String env) {
-            this.value = value == null ? "" : value;
-            this.env = env;
-        }
-
-        public PresetParameter(String value) { this(value, null); }
-
-        public String get() {
-            if (value != null)
-                return value;
-            if (env != null)
-                return System.getenv(env);
-            return "";
-        }
-
-        public static final PresetParameter EMPTY =
-            new PresetParameter("", null);
-
-        public static final Serializer<PresetParameter> SERIALIZER =
-            Serializer.STRING
-                .<PresetParameter>flatMap(
-                    p -> p.value, str -> new PresetParameter(str, null))
-                .or(Serializer.STRING.fieldOf("env").flatMap(
-                    p -> p.env, str -> new PresetParameter(null, str)));
-    }
-
     public static final Serializer<DatabasePreset> SERIALIZER =
         ObjectSerializer.create(
-            PresetParameter.SERIALIZER.entry("driver", dp -> dp.driver),
-            PresetParameter.SERIALIZER.entry("url", dp -> dp.url),
-            PresetParameter.SERIALIZER
+            Serializer.STRING.orEnv().entry("driver", dp -> dp.driver),
+            Serializer.STRING.orEnv().entry("url", dp -> dp.url),
+            Serializer.STRING.orEnv()
                 .<DatabasePreset>entry("username", dp -> dp.username)
-                .orElse(PresetParameter.EMPTY),
-            PresetParameter.SERIALIZER
+                .optional(),
+            Serializer.STRING.orEnv()
                 .<DatabasePreset>entry("password", dp -> dp.password)
-                .orElse(PresetParameter.EMPTY),
-            PresetParameter.SERIALIZER
+                .optional(),
+            Serializer.STRING.orEnv()
                 .<DatabasePreset>entry("database", dp -> dp.database)
-                .orElse(PresetParameter.EMPTY),
-            PresetParameter.SERIALIZER
+                .optional(),
+            Serializer.STRING.orEnv()
                 .<DatabasePreset>entry("table_prefix", dp -> dp.tablePrefix)
-                .orElse(PresetParameter.EMPTY),
+                .optional(),
             ConfigSection.SERIALIZER
                 .entry("parameters", DatabasePreset::parameters)
                 .orElse(new ConfigSection()),
--- api-sql/src/test/java/TestMariaDB.java
+++ api-sql/src/test/java/TestMariaDB.java
@@ -1,5 +1,9 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.wallentines.mdcfg.ConfigSection;
+import org.wallentines.mdcfg.serializer.ConfigContext;
 import org.wallentines.mdcfg.sql.DatabaseType;
+import org.wallentines.mdcfg.sql.PresetRegistry;
 import org.wallentines.mdcfg.sql.SQLConnection;
 
 public class TestMariaDB {
@@ -11,15 +15,57 @@ public class TestMariaDB {
         String user = System.getenv("TEST_MARIADB_USER");
         String pass = System.getenv("TEST_MARIADB_PASS");
 
-        if(url != null && user != null && pass != null) {
+        if (url != null && user != null && pass != null) {
 
             DatabaseType type = Common.getDBType("mariadb");
-            try(SQLConnection conn = type.create(url, user, pass)) {
+            try (SQLConnection conn = type.create(url, user, pass)) {
                 Common.testAll(conn);
             }
-
         }
-
     }
 
+    @Test
+    public void testPreset() {
+
+        String url = System.getenv("TEST_MARIADB_URL");
+        if (url == null)
+            return;
+
+        PresetRegistry reg =
+            PresetRegistry.SERIALIZER
+                .deserialize(
+                    ConfigContext.INSTANCE,
+                    new ConfigSection()
+                        .with("repository", new ConfigSection()
+                                                .with("type", "maven")
+                                                .with("folder", "drivers"))
+                        .with(
+                            "presets",
+                            new ConfigSection().with(
+                                "default",
+                                new ConfigSection()
+                                    .with("driver", "mariadb")
+                                    .with("url", new ConfigSection().with(
+                                                     "env", "TEST_MARIADB_URL"))
+                                    .with("username",
+                                          new ConfigSection().with(
+                                              "env", "TEST_MARIADB_USER"))
+                                    .with("password",
+                                          new ConfigSection().with(
+                                              "env", "TEST_MARIADB_PASS")))))
+                .getOrThrow();
+
+        try {
+            reg.connect(new ConfigSection().with("preset", "default"))
+                .thenAccept(Common::testAll)
+                .get();
+            reg.connect(new ConfigSection()
+                            .with("preset", "default")
+                            .with("table_prefix", "pre_"))
+                .thenAccept(Common::testAll)
+                .get();
+        } catch (Exception ex) {
+            Assertions.fail(ex);
+        }
+    }
 }
