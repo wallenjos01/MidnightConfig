6b0aacf252109d0e701f097c5210ed0def167a0e
wallenjos01 <wallenjos01@gmail.com>
2025-11-23 16:14:39 -0700

    Update minecraft module to 1.21.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,8 +13,8 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
-    implementation("com.gradleup.shadow:shadow-gradle-plugin:8.3.5")
+    implementation("com.gradleup.shadow:shadow-gradle-plugin:9.2.2")
     implementation("org.wallentines:gradle-multi-version:0.3.0")
     implementation("org.wallentines:gradle-patch:0.2.0")
-    implementation("net.fabricmc:fabric-loom:1.10-SNAPSHOT")
+    implementation("net.fabricmc:fabric-loom:1.13-SNAPSHOT")
 }
--- platform-minecraft/build.gradle.kts
+++ platform-minecraft/build.gradle.kts
@@ -14,9 +14,9 @@ Utils.setupResources(project, rootProject, "fabric.mod.json")
 
 dependencies {
 
-    minecraft("com.mojang:minecraft:1.21.6")
+    minecraft("com.mojang:minecraft:1.21.10")
     mappings(loom.officialMojangMappings())
-    modImplementation("net.fabricmc:fabric-loader:0.16.14")
+    modImplementation("net.fabricmc:fabric-loader:0.18.1")
 
     // Fabric API
     val apiModules = listOf(
@@ -24,7 +24,7 @@ dependencies {
         "fabric-lifecycle-events-v1"
     )
     for(mod in apiModules) {
-        modApi(fabricApi.module(mod, "0.127.0+1.21.6"))
+        modApi(fabricApi.module(mod, "0.138.3+1.21.10"))
     }
 
     api(project(":api"))
